13e97a43e74b1bf22e057b07dac566881a016a18
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-12-07 15:19:58 +0530

    storing data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    text: ""
+  };
+
+  handelAdd = async e =>{
+    await this.setState({
+      text : e.target.value
+    })
+  }
+  handelSubmit = e =>{
+    e.preventDefault();
+    console.log(this.state.text);
+    let formData = new FormData();
+    formData.append("text", this.state.text);
+    const url = "http://localhost:80/react-backend/";
+    axios.post(url, formData)
+    .then(res=> console.log(res.data))
+    .catch(err=> console.log(err));
+  }
+  render()
+  {
+    return (
+      <div className="App-header">
+        <input 
+        onChange = {this.handelAdd}
+        className="form-control" 
+        type="text" id="text" 
+        placeholder="Enter some text">
+        </input>
+        <br></br>
+        <button 
+        onClick = {this.handelSubmit}
+        className="btn-success" id="submit">Save</button>
+      </div>
+    );
+  }
 }
 
 export default App;
